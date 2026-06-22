c071b74313c807a13c0b217d32eab2dc6206804a
J M Tarun <tarunjm23@gmail.com>
2026-06-22 23:36:36 +0530

    vision-view-6679

--- funda.py
+++ funda.py
@@ -9,7 +9,7 @@ while True:
     if choice == '5': break
     if choice in ('1', '2', '3', '4'):
         try:
-            n1 = float(input("Num 1: ")); n2 = float(input("Num 2: "))
+            n1 = float(input("Num 1: ")); n2 = float(input("Num  "))
             ops = {'1': add, '2': subtract, '3': multiply, '4': divide}
             print("Result:", ops[choice](n1, n2))
         except ValueError: print("Invalid input.")
